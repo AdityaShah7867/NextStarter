5c521e00027d0777c7f94b315d194791b2396ab9
Aditya <192105adityashah@gmail.com>
2024-03-02 22:22:22 +0530

    initial

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { GoogleGeminiEffectDemo } from './test-new';
+
+export default function Home(){
+  return (
+    <main>
+      <GoogleGeminiEffectDemo />
+    </main>
+  );
+}
