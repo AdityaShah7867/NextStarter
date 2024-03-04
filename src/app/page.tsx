befaf724204e004011d23fb65792dae8af242b7f
Aditya <192105adityashah@gmail.com>
2024-03-04 23:06:50 +0530

    aded new pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import { GoogleGeminiEffectDemo } from './test-new';
+import React, { useState } from 'react'
+import { HeroScrollDemo } from './components/Land/Land'; 
+import { TypewriterEffectSmoothDemo } from './components/Land/HeroText';
+
 
 export default function Home(){
   return (
-    <main>
-      <GoogleGeminiEffectDemo />
+    <main className='bg-white'>
+      <TypewriterEffectSmoothDemo />
+      <HeroScrollDemo />
     </main>
   );
 }
